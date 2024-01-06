7ca58412179df9dc1a74b5e8d5cc85a20efe008f
naveen-kumar-v <naveengv75@gmail.com>
2024-01-06 16:04:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-gray-900">
+      <h1 className="text-white text-3xl">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
